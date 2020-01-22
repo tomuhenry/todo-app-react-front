97438df460d247c66af12b3466db6e36c142d80d
tomuhenry <tomusangehenry02@gmail.com>
2020-01-22 15:19:49 +0300

    React frontend todo app for personal practice

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+    return (
+      <React.Fragment>
+        <h1>About</h1>
+        <p>
+          This is Today mum woke up real angry at these national water mfs.
+          Niggas know only to deliver bills but not the service they bill for.{" "}
+        </p>
+      </React.Fragment>
+    );
+}
+
+export default About
